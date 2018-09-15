2e90fc51b7405b1ee5ebec45738c29653c919d9b
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2018-09-15 11:45:01 +0200

    controller v1

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,15 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public Rigidbody rb;
+
+	public float speed;
+
+	public float xSpeed;
+	public float ySpeed;
+	
+	private float x;
+	private float y;
+
+	public float minY;
+	public float maxY;
+
+	[Space] public Transform head;
+	
+	
+	
+	void Start ()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	void FixedUpdate()
+	{
+		CheckInputs();
+		
+		TurnBody();
+		
+		TurnHead();
+	}
+
+	void CheckInputs()
+	{
+		x = Input.GetAxis("Mouse X");
+		y = Input.GetAxis("Mouse Y");
+
+		if (Input.GetMouseButton(0))
+		{
+			Run();
+		}
+		else
+		{
+			Stop();
+		}
+	}
+	//tourner tout le corp
+	void TurnBody()
+	{
+		transform.Rotate(Vector3.up * x * xSpeed, Space.World);
+	}
+
+	//lever/baisser la tete, il faut clamper la rotation max
+	void TurnHead()
+	{
+		head.Rotate(Vector3.right*y * -ySpeed, Space.Self);
+		
+		float angle = head.localEulerAngles.x;
+		angle = (angle > 180) ? angle - 360 : angle;
+		
+		head.transform.localEulerAngles = new Vector3(Mathf.Clamp(angle, minY, maxY), 0,0);
+	}
+	
+	//RUN
+	void Run()
+	{
+		rb.velocity = transform.forward * speed;
+	}
+
+	void Stop()
+	{
+		rb.velocity = Vector3.zero;
 	}
 }
