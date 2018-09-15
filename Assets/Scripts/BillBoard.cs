44b94bea77f917b79b2f53be57cdde11a9c56028
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2018-09-15 16:25:31 +0200

    bobblehead

---
+++ Assets/Scripts/BillBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour {
+
+	public GameObject cam;
+
+	public bool yRotation;
+
+	private void Start()
+	{
+		cam = Camera.main.gameObject;
+	}
+
+	void Update () {
+		transform.LookAt(cam.transform.position);
+		
+		if (!yRotation)
+		{
+			transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);
+		}
+	}
+}
